1802e141d03e20b73260f866030479b26d07d0ca
João Victor Lopes Borges <jvlopesborges@gmail.com>
2025-11-29 02:39:05 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Lia eu amo você, se você me ama também, fale "eu hein"! 😎</h1>
+function Home() {
+  return <h1>Lia eu amo você, se você me ama também, fale "eu hein"! 😎</h1>;
 }
 
 export default Home;
