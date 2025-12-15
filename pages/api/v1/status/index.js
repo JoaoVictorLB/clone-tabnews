102f060325d5e6b02253134590a87a96b499244d
João Victor Lopes Borges <jvlopesborges@gmail.com>
2025-12-15 02:40:15 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,22 +3,30 @@ import database from "infra/database.js";
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
-  const postgresInfos = await database
-    .query(
-      "SELECT (SELECT version()) AS version, (SELECT setting::int FROM pg_settings WHERE name = 'max_connections') AS max_connections, (SELECT COUNT(*)::int FROM pg_stat_activity) AS current_users",
-    )
-    .then((result) => result.rows[0]);
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
-  let version = postgresInfos?.version.split(" ")[1];
-  version = version.toString();
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
 
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
-        version: version,
-        max_connections: postgresInfos.max_connections,
-        opened_connections: postgresInfos.current_users,
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
       },
     },
   });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,22 +3,6 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  expect(responseBody.updated_at).toBeDefined();
-  expect(responseBody.dependencies).toBeDefined();
-  expect(responseBody.dependencies.database).toBeDefined();
-  expect(responseBody.dependencies.database.version).toBeDefined();
-  expect(responseBody.dependencies.database.max_connections).toBeDefined();
-  expect(responseBody.dependencies.database.opened_connections).toBeDefined();
-
-  expect(typeof responseBody.dependencies).toBe("object");
-  expect(typeof responseBody.dependencies.database).toBe("object");
-  expect(typeof responseBody.dependencies.database.version).toBe("string");
-  expect(typeof responseBody.dependencies.database.max_connections).toBe(
-    "number",
-  );
-  expect(typeof responseBody.dependencies.database.opened_connections).toBe(
-    "number",
-  );
 
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
@@ -31,5 +15,5 @@ test("GET to /api/v1/status should return 200", async () => {
 
   const openedConnections =
     responseBody.dependencies.database.opened_connections;
-  expect(openedConnections).toEqual(7);
+  expect(openedConnections).toEqual(1);
 });
