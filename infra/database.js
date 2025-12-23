4d9ae2bce6330241d4b2728a225dc501222a41b1
João Victor Lopes Borges <jvlopesborges@gmail.com>
2025-12-23 03:02:55 +0000

    add config ssl for database with CA

--- infra/database.js
+++ infra/database.js
@@ -7,8 +7,9 @@ async function query(queryObject) {
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: getSSLValues(),
   });
+
   console.log("Credenciais do Postgres:", {
     database: process.env.POSTGRES_DB,
     host: process.env.POSTGRES_HOST,
@@ -32,3 +33,12 @@ async function query(queryObject) {
 export default {
   query: query,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+  return process.env.NODE_ENV === "development" ? true : true;
+}
